cb7ec41d3b386eba567185c1ab31851d365307da
Luuke <lukasz.bartosz1990@gmail.com>
2020-08-28 19:02:23 +0200

    Added handling board and neopixel modules import exception

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 print("TwitchPixel!")
 
-import board
-import neopixel
+try:
+    import board
+    import neopixel
+    IsRaspberryPi = True
+    print('Application running on Raspberry Pi')
+except (ImportError, NotImplementedError):
+    IsRaspberryPi = False
+    print('Application running on PC')
+
 import websocket
 import json
 from PubSubClient import PubSubClient
 
 twitchWebsocketsUrl = 'wss://pubsub-edge.twitch.tv'
+
+def setLed(r=0, g=0, b=0):
+    print(r,g,b)
+    
+    if IsRaspberryPi == True:
+        pixels = neopixel.NeoPixel(board.D18, 10)
+        
+        for j in range(9):
+            pixels[j] = (r,g,b)
+        pixels.show()
+
 
 with open('.env', 'r') as secretsFile:
     secretsJson = json.load(secretsFile)
@@ -15,7 +33,6 @@ with open('.env', 'r') as secretsFile:
 pubsub = PubSubClient(accessToken, ["channel-points-channel-v1.56618017"])
 pubsub.connect()
 
-pixels = neopixel.NeoPixel(board.D18, 10)
 
 while True:
     rcv = pubsub.receive()
@@ -36,7 +53,4 @@ while True:
     input = msg["data"]["redemption"]["user_input"]
     input = input.split()
 
-    for j in range(9):
-        pixels[j] = (int(input[0]),int(input[1]),int(input[2]))
-
-    pixels.show()
+    setLed(int(input[0]),int(input[1]),int(input[2]))
