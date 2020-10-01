a984c34a305134f3297be4440ba71cc5cdc0e78d
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-01 19:09:22 +0200

    Added set color method

--- LedStrip.py
+++ LedStrip.py
@@ -11,7 +11,14 @@ class LedStrip:
     def __init__(self, length=1):
         self._length = length
 
-        if IsRaspberryPi == true:
+        if IsRaspberryPi == True:
             pixels = neopixel.NeoPixel(board.D18, self._length)
-        else
+        else:
             pixels = None
+
+    def setColor(self, rgb=(0,0,0)):
+        print("setColor(): " + str(rgb))
+        for j in range(9):
+            pixels[j] = (min(abs(rgb[0]),255), min(abs(rgb[1]),255), min(abs(rgb[2]),255))
+        pixels.show()
+
