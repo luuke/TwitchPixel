66360329285fce2d89b23f33cd4b1b0f9d5ace57
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-01 19:10:50 +0200

    Using LedStrip class, removed commented code

--- main.py
+++ main.py
@@ -11,38 +11,30 @@ except (ImportError, NotImplementedError):
     print('Application running on PC')
 
 import asyncio
-# import websocket
 import json
 import re
+from LedStrip import LedStrip
 from PubSubClient import PubSubClient
 
 twitchWebsocketsUrl = 'wss://pubsub-edge.twitch.tv'
+ledStrip = LedStrip(10)
 
 def setLed(r=0, g=0, b=0):
-    print(r,g,b)
-    print(min(abs(r),255), min(abs(g),255), min(abs(b),255))
-    
-    if IsRaspberryPi == True:
-        for j in range(9):
-            pixels[j] = (min(abs(r),255), min(abs(g),255), min(abs(b),255))
-        pixels.show()
-
+    print("Setting RGB: " + r,g,b)
+    ledStrip.setColor((r,g,b))
 
 with open('.env', 'r') as secretsFile:
     secretsJson = json.load(secretsFile)
     accessToken = secretsJson['AccessToken']
 
-# pubsub = PubSubClient(accessToken, ["channel-points-channel-v1.56618017"])
-# pubsub.connect()
-
 async def main():
     while True:
         msg = await pubsub.getNextMessage()
-        print(msg)
+        print("main(): " + msg)
         msg = json.loads(msg)["data"]["message"]
         msg = json.loads(msg)["data"]["redemption"]["user_input"]
         colors = msg.split()
-        print(colors)
+        print("main(): " + str(colors))
         setLed(int(colors[0]),int(colors[1]),int(colors[2]))
 
 if __name__ == "__main__":
@@ -55,34 +47,3 @@ if __name__ == "__main__":
         asyncio.ensure_future(main())
     ]
     asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
-
-# while True:
-
-    
-#     rcv = pubsub.receive()
-#     pubsub.heartbeat()
-
-#     msg = json.loads(rcv)
-    
-#     msgType = msg["type"]
-#     redemptionTitle = msg["data"]["redemption"]["reward"]["title"]
-
-#     if msgType != "reward-redeemed":
-#         print("Invalid message type")
-#         print(msgType)
-#         continue
-
-#     if redemptionTitle != "[In development] Control LEDs":
-#         print("Invalid redemption type")
-#         continue
-
-#     userInput = msg["data"]["redemption"]["user_input"]
-
-#     rgb = re.search("[0-9]+ [0-9]+ [0-9]+", userInput)
-
-#     if rgb is None:
-#         print("Invalid user input: " + userInput)
-#     else:
-#         print("User input: " + userInput)
-#         colors = rgb[0].split()
-#         setLed(int(colors[0]),int(colors[1]),int(colors[2]))
